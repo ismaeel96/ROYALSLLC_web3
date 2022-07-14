5241838b6c578dbf93978e6c98b995a74a491901
ismaeel96 <jamil.ismaeel@gmail.com>
2022-07-14 18:55:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var path = require("path");
+
+var routes = require("./routes");
+
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json({limit: '1mb'}));
+app.use(routes);
+
+app.listen(app.get("port"), function(){
+    console.log("Server started on port ");
+});
+
+
+
+
+/*
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});*/
---
+++ routes.js
@@ -0,0 +1,75 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const throttle = require('fetch-throttle');
+
+const fetchThrottle = throttle(fetch, 3, 1000);
+
+var express = require("express");
+var router = express.Router();
+
+//import fetch from "node-fetch";
+//import {RateLimit} from "async-sema";
+const { Sema } = require('async-sema');
+const s = new Sema(
+  4, // Allow 4 concurrent async calls
+  {
+    capacity: 100 // Prealloc space for 100 tokens
+  }
+);
+
+
+router.get("/web3/", function(req, res) {
+res.render("index");
+});
+
+router.get('/web3/api/', function (req, res) {
+  console.log(req.query.apiCall);
+  //console.log("Age:", req.query.age);
+
+
+  fetchData(req.query.apiCall)
+      .then(function(serverPromise){
+        serverPromise.json()
+          .then(function(j) {
+            console.log(j);
+            res.send(j);
+          })
+          .catch(function(e){
+            console.log(e);
+          });
+      })
+      .catch(function(e){
+          console.log(e);
+        });
+});
+
+
+
+
+async function fetchData(x) {
+  //console.log(x);
+  await s.acquire()
+
+  try {
+    console.log(s.nrWaiting() + ' calls to fetch are waiting')
+    //console.log(x);
+    let ux = "--";
+    //let recobj=fetch(x);
+
+    const response = await fetchThrottle(x);
+    //console.log(response);
+    //const body = await response.text();
+
+    //const body = await response.json();
+
+    //console.log("");
+    //console.log(x);
+    //console.log(body);
+    return response;
+
+  } finally {
+    s.release();
+  }
+}
+
+
+module.exports = router;
