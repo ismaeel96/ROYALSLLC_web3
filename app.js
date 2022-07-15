b099e209be1976f48e94cf1fa71947d8111ac0ef
ismaeel96 <jamil.ismaeel@gmail.com>
2022-07-15 12:05:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.set("view engine", "ejs");
 
 app.use(express.json({limit: '1mb'}));
 app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
+
 
 app.listen(app.get("port"), function(){
     console.log("Server started on port ");
