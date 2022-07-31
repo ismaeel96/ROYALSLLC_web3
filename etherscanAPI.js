0af7d02bf8c0b1404a93c0691d5339fcfd096de2
ismaeel96 <jamil.ismaeel@gmail.com>
2022-07-31 04:05:45 -0400

    Add files via upload

--- etherscanAPI.js
+++ etherscanAPI.js
@@ -6,13 +6,19 @@ async function get_eth_mainnet_gas()
 {
 	while(true)
 	{
-		let myObject = await fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=WIXBRRT4DHHS6UM2CSJ4SB9GEU5SM5JBB5");
-		let myText = await myObject.text();
-		eth_mainnet_gas_object = JSON.parse(myText);
+		try {
+			let myObject = await fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=WIXBRRT4DHHS6UM2CSJ4SB9GEU5SM5JBB5");
+			let myText = await myObject.text();
+			eth_mainnet_gas_object = JSON.parse(myText);
 
-		//console.log(eth_mainnet_gas_object);
+			//console.log(eth_mainnet_gas_object);
 
-		await new Promise(resolve => setTimeout(resolve, 1500));
+			await new Promise(resolve => setTimeout(resolve, 2000));
+		} catch (e) {
+			console.log(e);
+		} finally {
+
+		}
 	}
 }
 
